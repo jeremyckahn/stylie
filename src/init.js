928f75af163bd6bea448711e8c927e3bd9e31117
Jeremy Kahn <jeremyckahn@gmail.com>
2012-02-27 19:50:08 -0800

    Using require.js to load things in a janky, janky way.

--- src/init.js
+++ src/init.js
@@ -1,1 +1,7 @@
+require(['src/css-gen', 'src/ease'], function () {
+  // Fancy schmancy way of getting global scope.
+  var global;
+  (function () { global = this; } (null));
 
+  global.app = {};
+});
