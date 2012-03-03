6e7716fda408774e34176982d236b06ca8a97e7c
Jeremy Kahn <jeremyckahn@gmail.com>
2012-03-03 15:40:17 -0800

    Moved the crosshair View into its own file.

--- src/ease.js
+++ src/ease.js
@@ -1,7 +1,9 @@
 require(['src/utils', 'src/css-gen', 'src/ui/checkbox', 'src/ui/button',
-        'src/ui/select', 'src/ui/auto-update-textfield', 'src/ui/ease-field'],
+        'src/ui/select', 'src/ui/auto-update-textfield', 'src/ui/ease-field',
+        'src/ui/crosshair'],
     function (utils, cssGen, checkbox, button,
-        select, autoUpdateTextfield, easeField) {
+        select, autoUpdateTextfield, easeField,
+        crosshair) {
 
   var app = {
     'config': {}
@@ -105,60 +107,16 @@ require(['src/utils', 'src/css-gen', 'src/ui/checkbox', 'src/ui/button',
     });
   app.kapi.canvas_style('background', '#eee');
 
-
-  var crosshairView = Backbone.View.extend({
-
-    // $.fn.draggable events don't propagate, so event delegation doesn't work.
-    // Drag event handlers must be bound in `initialize`.
-    'events': {}
-
-    ,'initialize': function (opts) {
-      _.extend(this, opts);
-      this.$el.draggable({
-        'containment': 'parent'
-        ,'drag': _.bind(this.onDrag, this)
-        ,'stop': _.bind(this.onDragStop, this)
-      });
-    }
-
-    ,'onDrag': function (evt, ui) {
-      var pos = this.$el.data('pos');
-      var timeToModify = pos === 'from' ? 0 : this.app.config.animationDuration;
-      this.app.config.circle.modifyKeyframe(timeToModify, this.getCenter());
-      this.app.kapi
-        .canvas_clear()
-        .redraw();
-      this.app.util.updatePath();
-    }
-
-    ,'onDragStop': function (evt, ui) {
-      this.onDrag.apply(this, arguments);
-    }
-
-    ,'getCenter': function () {
-      var pos = this.$el.position();
-      return {
-        x: pos.left + this.$el.width()/2
-        ,y: pos.top + this.$el.height()/2
-      };
-
-    }
-
-  });
-
   app.config.crosshairs = {
-    'from': new crosshairView({
+    'from': new crosshair.view({
         'app': app
         ,'$el': $('.crosshair.from')
       })
-    ,'to': new crosshairView({
+    ,'to': new crosshair.view({
         'app': app
         ,'$el': $('.crosshair.to')
       })
   };
-
-
-
 
   app.kapi.addActor(app.config.circle);
   app.config.circle.keyframe(0,
