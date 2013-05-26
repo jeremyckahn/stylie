52378043c55bb4eba147c967dc12148619d9b56e
Jeremy Kahn <jeremyckahn@gmail.com>
2013-05-25 17:29:25 -0700

    Actually adds the constants module.

---
+++ src/constants.js
@@ -0,0 +1,10 @@
+define(function () {
+  return {
+    'PRERENDER_GRANULARITY': 150
+    ,'RENDER_GRANULARITY': 100
+    ,'KEYFRAME_UPDATED': 'keyframeUpdated'
+    ,'UPDATE_CSS_OUTPUT': 'updateCSSOutput'
+    ,'TOGGLE_FADE_SPEED': 200
+    ,'INITIAL_KEYFRAMES': 2
+  };
+});
