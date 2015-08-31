e977109ffaefd5bb42222b8123af0e1b19d03f2f
Jeremy Kahn <jeremyckahn@gmail.com>
2015-08-30 21:21:51 -0500

    Refactor some event handlers into lateralusEvents

--- app/scripts/components/keyframes-panel/view.js
+++ app/scripts/components/keyframes-panel/view.js
@@ -32,52 +32,49 @@ define([
     }
 
     /**
+     * @param {KeyframePropertyCollection} collection
+     */
+    ,lateralusEvents: {
+      /**
+       * @param {Backbone.Model} keyframePropertyModel
+       */
+      keyframePropertyAdded: function (keyframePropertyModel) {
+        var keyframeFormComponent = this.addComponent(KeyframeFormComponent, {
+          model: keyframePropertyModel
+        });
+
+        this.keyframeFormComponents.push(keyframeFormComponent);
+
+        // TODO: This is a weird pattern.  Maybe use a proper Model/Collection
+        // pattern that automatically cleans itself up?
+        this.listenTo(keyframeFormComponent, 'beforeDispose', function () {
+          this.keyframeFormComponents =
+            _.without(this.keyframeFormComponents, keyframeFormComponent);
+        }.bind(this));
+
+        this.$keyframesList.append(keyframeFormComponent.view.el);
+      }
+
+      ,confirmNewKeyframeOrder: function (collection) {
+        this.$keyframesList.children().detach();
+
+        collection.each(function (model) {
+          var keyframeFormComponent = _.find(this.keyframeFormComponents,
+              function (keyframeFormComponent) {
+            return keyframeFormComponent.view.model === model;
+          });
+
+          this.$keyframesList.append(keyframeFormComponent.view.el);
+        }, this);
+      }
+    }
+
+    /**
      * @param {Object} [options] See http://backbonejs.org/#View-constructor
      */
     ,initialize: function () {
       baseProto.initialize.apply(this, arguments);
       this.keyframeFormComponents = [];
-
-      this.listenFor(
-        'keyframePropertyAdded', this.onKeyframePropertyAdded.bind(this));
-      this.listenFor(
-        'confirmNewKeyframeOrder', this.onConfirmNewKeyframeOrder.bind(this));
-    }
-
-    /**
-     * @param {Backbone.Model} keyframePropertyModel
-     */
-    ,onKeyframePropertyAdded: function (keyframePropertyModel) {
-      var keyframeFormComponent = this.addComponent(KeyframeFormComponent, {
-        model: keyframePropertyModel
-      });
-
-      this.keyframeFormComponents.push(keyframeFormComponent);
-
-      // TODO: This is a weird pattern.  Maybe use a proper Model/Collection
-      // pattern that automatically cleans itself up?
-      this.listenTo(keyframeFormComponent, 'beforeDispose', function () {
-        this.keyframeFormComponents =
-          _.without(this.keyframeFormComponents, keyframeFormComponent);
-      }.bind(this));
-
-      this.$keyframesList.append(keyframeFormComponent.view.el);
-    }
-
-    /**
-     * @param {KeyframePropertyCollection} collection
-     */
-    ,onConfirmNewKeyframeOrder: function (collection) {
-      this.$keyframesList.children().detach();
-
-      collection.each(function (model) {
-        var keyframeFormComponent = _.find(this.keyframeFormComponents,
-            function (keyframeFormComponent) {
-          return keyframeFormComponent.view.model === model;
-        });
-
-        this.$keyframesList.append(keyframeFormComponent.view.el);
-      }, this);
     }
   });
 
