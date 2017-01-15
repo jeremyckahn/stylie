2e05827dac8110e93bb832bdadcbf7e0b6a65d9b
Jeremy Kahn <jeremyckahn@gmail.com>
2017-01-14 18:04:32 -0600

    Remove unused Grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,11 +1,5 @@
 // jshint maxlen:100
 'use strict';
-var LIVERELOAD_PORT = 35731;
-var SERVER_PORT = 9005;
-var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
-var mountFolder = function (connect, dir) {
-  return connect.static(require('path').resolve(dir));
-};
 
 // # Globbing
 // for performance reasons we're only matching one level down:
@@ -28,55 +22,6 @@ module.exports = function (grunt) {
 
   grunt.initConfig({
     yeoman: yeomanConfig,
-    watch: {
-      options: {
-        nospawn: true,
-        livereload: LIVERELOAD_PORT
-      },
-      livereload: {
-        options: {
-          livereload: grunt.option('livereloadport') || LIVERELOAD_PORT
-        },
-        files: [
-          '<%= yeoman.app %>/*.html',
-          '<%= yeoman.app %>/scripts/**/*.{js,mustache}',
-          '<%= yeoman.app %>/node_modules/aenima/**/*.{js,mustache}',
-          '<%= yeoman.app %>/img/{,*/}*.{png,jpg,jpeg,gif,webp}',
-          '<%= yeoman.app %>/scripts/templates/*.{ejs,mustache,hbs}'
-        ]
-      },
-    },
-    connect: {
-      options: {
-        port: grunt.option('port') || SERVER_PORT,
-        // change this to '0.0.0.0' to access the server from outside
-        hostname: 'localhost'
-      },
-      livereload: {
-        options: {
-          middleware: function (connect) {
-            return [
-              lrSnippet,
-              mountFolder(connect, yeomanConfig.app)
-            ];
-          }
-        }
-      },
-      dist: {
-        options: {
-          middleware: function (connect) {
-            return [
-              mountFolder(connect, yeomanConfig.dist)
-            ];
-          }
-        }
-      }
-    },
-    open: {
-      server: {
-        path: 'http://localhost:<%= connect.options.port %>'
-      }
-    },
     clean: {
       dist: ['<%= yeoman.dist %>/*']
     },
@@ -180,23 +125,6 @@ module.exports = function (grunt) {
     exec: {
       webpack: './node_modules/.bin/webpack -d --optimize-minimize'
     }
-  });
-
-  grunt.registerTask('server', function (target) {
-    grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
-    grunt.task.run(['serve' + (target ? ':' + target : '')]);
-  });
-
-  grunt.registerTask('serve', function (target) {
-    if (target === 'dist') {
-      return grunt.task.run(['build', 'open:server', 'connect:dist:keepalive']);
-    }
-
-    grunt.task.run([
-      'connect:livereload',
-      'open:server',
-      'watch'
-    ]);
   });
 
   grunt.registerTask('build', [
