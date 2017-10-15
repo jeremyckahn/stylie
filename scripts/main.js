bcc7d0571a1b7397db302fec5bb5f9c016904dbe
Jeremy Kahn <jeremyckahn@gmail.com>
2017-10-14 23:31:08 -0500

    Move style booting to consumer

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,4 @@
+import '../styles/main.sass';
 import Stylie from './stylie';
 
 window.stylie = new Stylie(document.getElementById('stylie'));
--- scripts/stylie.js
+++ scripts/stylie.js
@@ -16,8 +16,6 @@ define([
 
   ,'./constant'
 
-  ,'../styles/main.sass'
-
 ], function (
 
   _
